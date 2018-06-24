f2a2fe80c2a710092c196528ec3d814416396219
anzai <hogehoge@hogehoge.mail.com>
2018-06-24 18:26:09 +0900

    hello.pyとindex.html更新

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', title='test', userName='kentei')
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET','POST'])
 def hello():
-    name = "Hello world!"
-    return name
+    if request.method == 'POST':
+        name = request.form['name']
+    else:
+        name = "no name."
+    return name + " Hello world!"
 
 if __name__ == "__main__":
     app.run()
